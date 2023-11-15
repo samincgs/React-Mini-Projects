2b8a0f72f5b19b4ad175cf8a5d09c47f2c2e993c
samin <samincgs@gmail.com>
2023-11-15 05:02:55 -0500

    added some css will go back to working on functionality

--- reviews/src/data.js
+++ reviews/src/data.js
@@ -1,7 +1,7 @@
 import image1 from './images/image-1.jpg';
-import image2 from './images/image-1.jpg';
-import image3 from './images/image-1.jpg';
-import image4 from './images/image-1.jpg';
+import image2 from './images/image-2.jpg';
+import image3 from './images/image-3.jpg';
+import image4 from './images/image-4.jpg';
 
 export const reviews = [
   {
