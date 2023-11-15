aa92fbf4b793d3a41b0926fc5eaa7dcaeaf9a05f
samin <samincgs@gmail.com>
2023-11-14 23:29:49 -0500

    users can now hide or expand the description

--- tours/src/components/Tour.jsx
+++ tours/src/components/Tour.jsx
@@ -1,11 +1,24 @@
+import { useState } from 'react';
+
 const Tour = ({ id, image, info, name, price, removeTour }) => {
+  const [showText, setShowText] = useState(false);
+
   return (
     <div className='single-tour'>
       <img src={image} alt={name} className='img' />
       <span className='tour-price'>$ {price}</span>
       <div className='tour-info'>
         <h3>{name}</h3>
-        <p>{info}</p>
+        <p>
+          {showText ? info : `${info.substring(0, 200)}...`}
+          <button
+            type='button'
+            className='toggle-btn'
+            onClick={() => setShowText(!showText)}
+          >
+            {showText ? 'hide text' : 'read more'}
+          </button>
+        </p>
         <button type='button' className='btn' onClick={() => removeTour(id)}>
           not interested
         </button>
