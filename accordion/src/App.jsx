2ee270ffce79c625d6fb8bd63beba38ea98493f1
samin <samincgs@gmail.com>
2023-11-16 01:21:54 -0500

    created data file and updated title

--- accordion/src/App.jsx
+++ accordion/src/App.jsx
@@ -1,4 +1,11 @@
+import { useState } from 'react';
+import { data } from './data.js';
+
 function App() {
+  const [questions, setQuestions] = useState(data);
+
+  console.log(questions);
+
   return (
     <>
       <p>Hello react</p>
