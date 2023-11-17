293c17a0679f1a6e3ae1b5c3c623faa1edeb48c7
samin <samincgs@gmail.com>
2023-11-17 04:08:35 -0500

    functionality fully completed, now need to implemented css

--- menu/src/App.jsx
+++ menu/src/App.jsx
@@ -3,16 +3,27 @@ import { data } from './data.js';
 
 import Title from './components/Title.jsx';
 import Menu from './components/Menu.jsx';
+import Categories from './components/Categories.jsx';
 
 const currCategories = ['All', ...new Set(data.map((item) => item.category))];
 
 function App() {
   const [menu, setMenuItems] = useState(data);
-  const [categories, setCategories] = useState([currCategories]);
+  const [categories, setCategories] = useState(currCategories);
+
+  const findMenu = (category) => {
+    if (category === 'All') {
+      setMenuItems(menu);
+      return;
+    }
+    const categoryMenu = data.filter((menu) => menu.category === category);
+    setMenuItems(categoryMenu);
+  };
 
   return (
     <main>
       <Title text='our menu' />
+      <Categories categories={categories} findMenu={findMenu} />
       <Menu menu={menu} />
     </main>
   );
--- menu/src/components/Categories.jsx
+++ menu/src/components/Categories.jsx
@@ -1,4 +1,19 @@
-const Categories = () => {
-  return <div>Categories</div>;
+const Categories = ({ categories, findMenu }) => {
+  return (
+    <div className='btn-container'>
+      {categories.map((category, index) => {
+        return (
+          <button
+            type='button'
+            className='btn'
+            key={index}
+            onClick={() => findMenu(category)}
+          >
+            {category}
+          </button>
+        );
+      })}
+    </div>
+  );
 };
 export default Categories;
