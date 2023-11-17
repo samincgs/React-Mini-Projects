7883520446e9f574c696e09e8f1108b561bf5526
samin <samincgs@gmail.com>
2023-11-17 05:21:22 -0500

    project is now completed!

--- menu/src/components/MenuItem.jsx
+++ menu/src/components/MenuItem.jsx
@@ -4,8 +4,8 @@ const MenuItem = ({ title, price, img, desc }) => {
       <img src={img} alt={title} className='img' />
       <div className='info-content'>
         <div className='info-center'>
-          <h4>{title}</h4>
-          <span>${price}</span>
+          <h4 className='title'>{title}</h4>
+          <span className='info-price'>${price}</span>
         </div>
         <p className='desc'>{desc}</p>
       </div>
