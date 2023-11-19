765372d328ee1a7ae3eeaa123835fd0ea604151d
samin <samincgs@gmail.com>
2023-11-19 00:05:53 -0500

    did the Job and Duties component and the css

--- tabs/src/components/Jobs.jsx
+++ tabs/src/components/Jobs.jsx
@@ -4,15 +4,11 @@ const Jobs = ({ jobs }) => {
   const { title, company, dates, duties } = jobs[0];
 
   return (
-    <div className='job-container'>
-      <div className='job-info'>
-        <h2>{title}</h2>
-        <span>{company}</span>
-        <h4>{dates}</h4>
-        <div className='job-duties'>
-          <Duties duties={duties} />
-        </div>
-      </div>
+    <div className='job-info'>
+      <h2>{title}</h2>
+      <span className='job-company'>{company}</span>
+      <h4>{dates}</h4>
+      <Duties duties={duties} />
     </div>
   );
 };
