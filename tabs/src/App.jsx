6ef59d00a9581a114a652ce2d7c192837f767f36
samin <samincgs@gmail.com>
2023-11-18 20:27:59 -0500

    Added a loading screen for users and added the css for the loading circle

--- tabs/src/App.jsx
+++ tabs/src/App.jsx
@@ -1,5 +1,37 @@
+import { useEffect, useState } from 'react';
+
+import Loading from './components/Loading.jsx';
+
+const url = 'https://course-api.com/react-tabs-project';
 function App() {
-  return <div>hello react</div>;
+  const [jobs, setJobs] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchJobs = async () => {
+    try {
+      const response = await fetch(url);
+      const jobData = await response.json();
+
+      setJobs(jobData);
+    } catch (error) {
+      console.log(error);
+    }
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  return (
+    <main>
+      <div className='jobs-container'>hello react</div>
+    </main>
+  );
 }
 
 export default App;
