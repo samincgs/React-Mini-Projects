87c2346807a7d0da362e76afffc4f6db6b1263d1
samin <samincgs@gmail.com>
2023-11-18 20:42:51 -0500

    created the whole main job layout now gonna add css

--- tabs/src/components/Duties.jsx
+++ tabs/src/components/Duties.jsx
@@ -1,15 +1,21 @@
+import { FaAngleDoubleRight } from 'react-icons/fa';
+import { v4 as uuidv4 } from 'uuid';
+
 const Duties = ({ duties }) => {
   console.log(duties);
   return (
-    <div>
+    <>
       {duties.map((duty) => {
         return (
-          <div className='job-desc'>
-            <p>{duty}</p>
+          <div key={uuidv4()} className='job-desc'>
+            <span className='job-icon'>
+              <FaAngleDoubleRight />
+            </span>
+            <p className='job-duty'>{duty}</p>
           </div>
         );
       })}
-    </div>
+    </>
   );
 };
 export default Duties;
