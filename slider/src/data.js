ee7900604c05d63f3f890b5c1db0357f9d208829
samin <samincgs@gmail.com>
2023-11-22 02:17:36 -0500

    imported images and created a data file

---
+++ slider/src/data.js
@@ -0,0 +1,45 @@
+import john from './images/john.jpeg';
+import lelia from './images/lelia.jpeg';
+import jane from './images/jane.jpg';
+import alex from './images/alex.jpeg';
+
+export const shortList = [
+  {
+    id: 1,
+    image: lelia,
+    name: 'Lelia Burton',
+    job: 'Graphic Designer',
+    desc: 'leaving single spirit officer noon simplest caught explanation percent brass addition connected pretty pay substance kind over angle object we factory labor no cream',
+  },
+];
+
+export const list = [
+  {
+    id: 1,
+    image: lelia,
+    name: 'Lelia Burton',
+    job: 'Graphic Designer',
+    desc: 'Leaving single spirit officer noon simplest caught explanation percent brass addition connected pretty pay substance kind over angle object we factory labor no cream',
+  },
+  {
+    id: 2,
+    image: john,
+    name: 'John Doe',
+    job: 'Financial Analyst',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam.',
+  },
+  {
+    id: 3,
+    image: jane,
+    name: 'Jane Smith',
+    job: 'Architect',
+    desc: 'Ut eget nunc ac justo dignissim lacinia. Nullam varius turpis et tellus luctus, eu gravida ligula accumsan.',
+  },
+  {
+    id: 4,
+    image: alex,
+    name: 'Alex Johnson',
+    job: 'Content Writer',
+    desc: 'In hac habitasse platea dictumst. Suspendisse potenti. Nulla facilisi. Pellentesque non lacus ac mi dignissim condimentum.',
+  },
+];
