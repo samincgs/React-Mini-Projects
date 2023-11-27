faa219f4ab716a6ee655d6ddf58bd5d4e5a20247
samin <samincgs@gmail.com>
2023-11-27 01:53:49 -0500

    all functionality and css has been added

--- color-generator/src/components/SingleColor.jsx
+++ color-generator/src/components/SingleColor.jsx
@@ -1,7 +1,24 @@
+import { toast } from 'react-toastify';
+
 const SingleColor = ({ color, index }) => {
   const { hex, weight } = color;
+
+  const copyColor = async () => {
+    if (navigator.clipboard) {
+      try {
+        await navigator.clipboard.writeText(`#${hex}`);
+        toast.success('Color has been copied to clipboard');
+      } catch (error) {
+        toast.error('Color cannot be copied to clipboard');
+      }
+    } else {
+      toast.error('Color cannot be copied to clipboard');
+    }
+  };
+
   return (
     <div
+      onClick={copyColor}
       className={index > 10 ? 'color color-light' : 'color'}
       style={{ backgroundColor: `#${hex}` }}
     >
