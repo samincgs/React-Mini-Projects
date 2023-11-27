dd7b345a465debcaa0d5207de8126800a9b68c1d
samin <samincgs@gmail.com>
2023-11-27 17:53:35 -0500

    grocery form design is done

--- grocery-bud/src/components/GroceryForm.jsx
+++ grocery-bud/src/components/GroceryForm.jsx
@@ -2,8 +2,14 @@ import { useState } from 'react';
 
 const GroceryForm = () => {
   const [item, setItem] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(item);
+  };
+
   return (
-    <form className='form-grocery'>
+    <form className='form-grocery' onSubmit={handleSubmit}>
       <h4>grocery bud</h4>
       <div className='form-input'>
         <input
@@ -11,6 +17,9 @@ const GroceryForm = () => {
           value={item}
           onChange={(e) => setItem(e.target.value)}
         />
+        <button type='submit' className='btn'>
+          add item
+        </button>
       </div>
     </form>
   );
