5ee93f129cf52923ad86a2ff9abee9c03357c1f5
samin <samincgs@gmail.com>
2023-11-27 19:24:28 -0500

    data is now stored in local storage

--- grocery-bud/src/App.jsx
+++ grocery-bud/src/App.jsx
@@ -1,12 +1,23 @@
 import { ToastContainer, toast } from 'react-toastify';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { nanoid } from 'nanoid';
 
 import GroceryForm from './components/GroceryForm.jsx';
 import DisplayItem from './components/DisplayItem.jsx';
 
+const saveLocalStorage = (items) => {
+  localStorage.setItem('list', JSON.stringify(items));
+};
+
 function App() {
   const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const storedItems = JSON.parse(localStorage.getItem('list'));
+    if (storedItems) {
+      setItems(storedItems);
+    }
+  }, []);
 
   const addItem = (item) => {
     const finalItem = {
@@ -16,11 +27,13 @@ function App() {
     };
 
     setItems([...items, finalItem]);
+    saveLocalStorage([...items, finalItem]);
   };
 
   const deleteItem = (id) => {
     const finalItem = items.filter((item) => item.id !== id);
     setItems(finalItem);
+    saveLocalStorage(finalItem);
     toast.success('Item has now been deleted');
   };
 
